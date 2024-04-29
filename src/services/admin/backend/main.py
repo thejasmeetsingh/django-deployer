ddde7dea4c88c8781ca5f64b1990c5124d03c7ea
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-04-29 17:44:46 +0530

    Updated main.py

--- src/services/admin/backend/main.py
+++ src/services/admin/backend/main.py
@@ -1,9 +1,34 @@
-from fastapi import FastAPI
+"""
+Backend Application Main EntryPoint
+"""
+
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+
+from auth import routes as auth_routes
 
 
-app = FastAPI()
+def get_app() -> FastAPI:
+    _app = FastAPI()
+
+    _app.title = "Django Deployer"
+    _app.description = "Admin Backend"
+
+    prefix = "/api/v1"
+
+    _app.include_router(auth_routes.router, prefix=prefix)
+
+    return _app
 
 
-@app.get("/")
-def hello_world():
-    return {"Hello": "World"}
+app = get_app()
+
+
+# Health Check Route
+class HealthCheck(BaseModel):
+    message: str
+
+
+@app.get("/health-check/", status_code=status.HTTP_200_OK, response_class=HealthCheck)
+async def health_check():
+    return HealthCheck(message="Admin backend is up & running!")
