8884ac8329f6a8c479b6801ca6bbd79a04f09bd3
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-04-29 15:34:04 +0530

    Added Instance model migrations

---
+++ src/services/admin/backend/migrations/versions/26154e35f873_create_instance_table.py
@@ -0,0 +1,30 @@
+"""create instance table
+
+Revision ID: 26154e35f873
+Revises: 
+Create Date: 2024-04-29 15:30:58.966436
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '26154e35f873'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "instances",
+        sa.Column("id", sa.UUID, primary_key=True, index=True),
+        sa.Column("type", sa.String, nullable=False, unique=True)
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("instances")
