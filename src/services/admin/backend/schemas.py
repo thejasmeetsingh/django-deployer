7b3d3b17e106841db3d0ea3396a87a8c60650ce2
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-04-29 16:55:47 +0530

    Added pydantic schemas

---
+++ src/services/admin/backend/schemas.py
@@ -0,0 +1,79 @@
+"""
+Pydantic Schemas for data validation and DB models type annotations
+"""
+
+from datetime import datetime
+
+from pydantic import BaseModel, Field, UUID4
+
+
+class Instance(BaseModel):
+    id: UUID4
+    created_at: datetime
+    modified_at: datetime
+
+    type: str
+
+    class Config:
+        from_attributes = True
+
+
+class Plan(BaseModel):
+    id: UUID4
+    created_at: datetime
+    modified_at: datetime
+
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str = Field(min_length=8)
+
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
+
+
+class Token(BaseModel):
+    access: str
+    refresh: str
+
+
+class TokenResponse(BaseModel):
+    message: str
+    data: Token
+
+
+class InstanceRequest(BaseModel):
+    type: str = Field(max_length=50)
+
+
+class InstanceResponse(BaseModel):
+    message: str
+    data: Instance
+
+
+class InstanceListResponse(BaseModel):
+    results: list[Instance]
+
+
+class PlanCreateRequest(BaseModel):
+    name: str = Field(max_length=5)
+    instance_id: UUID4
+
+
+class PlanUpdateRequest(BaseModel):
+    name: str = Field(max_length=5)
+
+
+class PlanResponse(BaseModel):
+    message: str
+    data: Plan
+
+
+class PlanListResponse(BaseModel):
+    results: list[Plan]
