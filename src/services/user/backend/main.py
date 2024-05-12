3b678881979b4ca52ade0d878187155c7ba268ec
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2024-05-12 12:10:05 +0000

    Updated routes and added logging

--- src/services/user/backend/main.py
+++ src/services/user/backend/main.py
@@ -1,16 +1,21 @@
 import uuid
+import traceback
 from typing import Annotated
 
 import redis.asyncio as redis
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, Depends, HTTPException, status
 
 import env
+from logger import get_logger
 from schemas import HealthCheck, DeployRequest, DeployResponse, PlanResponse
+from message_queue.client import get_plans as get_cached_plans
 
 
 app = FastAPI()
 app.title = "Django Deployer"
 app.description = "User Service"
+
+logger = get_logger(__name__)
 
 
 async def get_redis():
@@ -34,19 +39,40 @@ async def health_check():
 
 @app.get(path="/api/v1/plan/", response_model=list[PlanResponse], status_code=status.HTTP_200_OK)
 async def get_plans(_redis: Annotated[redis.Redis, Depends(get_redis)]):
-    pass
+    try:
+        # Fetch plan data from redis
+        plans: list[dict[str, str]] = await get_cached_plans(_redis)
+
+        return [PlanResponse(plan=plan["plan"], instance=plan["instance"]) for plan in plans]
+
+    except Exception as e:
+        logger.error({
+            "error": e,
+            "traceback": traceback.format_exc()
+        })
+
+        raise HTTPException(detail="Something went wrong", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) from e
 
 
 @app.post(path="/api/v1/deploy/", response_model=DeployResponse, status_code=status.HTTP_200_OK)
 async def deploy(deploy_request: DeployRequest, _redis: Annotated[redis.Redis, Depends(get_redis)]):
-    _id = str(uuid.uuid4())
-    data = {
-        "email": deploy_request.email,
-        "plan": deploy_request.plan,
-        "instance": deploy_request.instance,
-        "is_deployed": False
-    }
+    try:
+        _id = str(uuid.uuid4())
+        data = {
+            "email": deploy_request.email,
+            "plan": deploy_request.plan,
+            "instance": deploy_request.instance,
+            "is_deployed": False
+        }
 
-    # Store deployment data in DB
-    await _redis.hset(_id, mapping=data)
-    return DeployResponse(message="Deployment request received")
+        # Store deployment data in DB
+        await _redis.hset(_id, mapping=data)
+        return DeployResponse(message="Deployment request received")
+    
+    except Exception as e:
+        logger.error({
+            "error": e,
+            "traceback": traceback.format_exc()
+        })
+
+        raise HTTPException(detail="Something went wrong", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR) from e
