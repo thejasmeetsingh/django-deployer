e93b158bc88791d0eb151dd595b8d1d48a76bc8b
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-06-11 16:31:44 +0530

    Added context for instance and plans

---
+++ src/services/admin/frontend/src/context/plan.jsx
@@ -0,0 +1,55 @@
+import { createContext, useState } from "react";
+
+const PlanContext = createContext();
+
+function PlanProvider({ children }) {
+  // ToDo: Replace static data manipulation with API calls
+  const [plans, setPlan] = useState([]);
+
+  const fetchPlans = async () => {
+    setPlan([
+      { id: 1, name: "plan 1" },
+      { id: 2, name: "plan 2" },
+      { id: 3, name: "plan 3" },
+    ]);
+  };
+
+  const createPlan = async (name) => {
+    setPlan([...plans, { id: plans.length + 1, name }]);
+  };
+
+  const editPlan = async (id, name) => {
+    setPlan(
+      plans.map((plan, index) => {
+        if (index === id) {
+          return { ...plan, name };
+        }
+        return plan;
+      })
+    );
+  };
+
+  const deletePlan = async (id) => {
+    setPlan(
+      plans.filter((_, index) => {
+        return index !== id;
+      })
+    );
+  };
+
+  return (
+    <PlanContext.Provider
+      value={{
+        plans,
+        fetchPlans,
+        createPlan,
+        editPlan,
+        deletePlan,
+      }}
+    >
+      {children}
+    </PlanContext.Provider>
+  );
+}
+
+export { PlanContext, PlanProvider };
